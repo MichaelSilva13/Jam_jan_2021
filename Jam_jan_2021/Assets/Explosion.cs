9aa766e0f14f4517ecd97d3da6189ece61c9db5e
Michael <michael_silva13@hotmail.com>
2021-01-16 11:19:40 -0500

    Added particles and shooting advanced

---
+++ Jam_jan_2021/Assets/Explosion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public ParticleSystem part;
+    private SphereCollider _collider;
+
+    void OnEnable()
+    {
+        part = GetComponent<ParticleSystem>();
+        if(!_collider)
+            _collider = GetComponent<SphereCollider>();
+        _collider.enabled = false;
+    }
+
+    public IEnumerator explode()
+    {
+        _collider.enabled = true;
+        float targetRadius = 20f, currRadius = 0f;
+        float time = 0.0f;
+        while (time < 0.5f)
+        {
+            _collider.radius = currRadius;
+            time += Time.fixedDeltaTime;
+            currRadius = Mathf.SmoothStep(0, targetRadius, time / 0.5f);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+        time = 0.0f;
+        currRadius = 20f;
+        while (time < 0.5f)
+        {
+            _collider.radius = currRadius;
+            time += Time.fixedDeltaTime;
+            currRadius = Mathf.SmoothStep(targetRadius, 0, time / 0.5f);
+            yield return new WaitForSeconds(Time.fixedDeltaTime);
+        }
+
+        _collider.radius = 20f;
+        yield return new WaitForSeconds(0.05f);
+        _collider.enabled = false;
+    }
+}
---
+++ Jam_jan_2021/Assets/Scripts/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float sdTime = 1f;
+
+    private Collider _collider;
+    [SerializeField]
+    private ParticleSystem _sparkParticle;
+
+    private ParticleSystem _particle;
+    private Coroutine SDCoroutine;
+    private Rigidbody _rigidbody;
+    private MeshRenderer _meshRenderer;
+    void OnEnable()
+    {
+        if (!_collider)
+            _collider = GetComponent<Collider>();
+        if (!_rigidbody)
+            _rigidbody = GetComponent<Rigidbody>();
+        if (!_particle)
+            _particle = GetComponent<ParticleSystem>();
+        if (!_meshRenderer)
+            _meshRenderer = GetComponent<MeshRenderer>();
+        
+        _particle.Play();
+        _sparkParticle.Stop();
+        _meshRenderer.enabled = true;
+        _collider.enabled = true;
+        SDCoroutine = StartCoroutine(SelfDestruct());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StopCoroutine(SDCoroutine);
+        StartCoroutine(Sparks());
+    }
+
+    IEnumerator Sparks()
+    {
+        _meshRenderer.enabled = false;
+        _particle.Stop();
+        _rigidbody.velocity = Vector3.zero;
+        _collider.enabled = false;
+        _sparkParticle.Play();
+        yield return new WaitForSeconds(sdTime);
+        GameObjectPoolController.Enqueue(GetComponent<Poolable>());
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(sdTime);
+        StartCoroutine(Sparks());
+    }
+}
